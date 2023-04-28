3f1d13abe83d4ea263b6197e157b574cdf8ab7ce
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-04-28 15:32:16 +0100

    updated branch

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Button, StyleSheet, Text, View } from "react-native";
+import { testPost } from "./utils/dbApi";
 import { testPost } from "./utils/dbApi";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="test" onPress={testPost} />
       <StatusBar style="auto" />
     </View>
   );
