2aaa29726d1abd9b4043206f1440b83358b69f87
ah28011992 <hedgecockaaron@gmail.com>
2023-04-29 01:10:37 +0100

    add sign up form with navigator functionality

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { testPost } from "./utils/dbApi";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MyStack from "./Routes/MyStack";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="test" onPress={testPost} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ Screens/Homescreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function Homescreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Hello world</Text>
+        </View>
+    );
+}
