908e2b9f662a38cfd7d59723be26085eea67d2b7
Kendrick Chan <kenchan0824@gmail.com>
2023-05-02 10:59:52 +0100

    add onSuccess method

--- Screens/SignIn.jsx
+++ Screens/SignIn.jsx
@@ -6,27 +6,28 @@ import { UserInfoContext } from "../contexts/UserInfo";
 
 function SignIn() {
   const { userInfo, setUserInfo } = useContext(UserInfoContext);
-  const [user, setUser] = useState(null);
   const [request, token, promptAsyn] = useGoogle();
   const navigation = useNavigation();
+
+  function onSuccess(user) {
+    setUserInfo({
+      uid: user.id,
+      email: user.email,
+      family_name: user.family_name,
+      given_name: user.given_name,
+      name: user.name,
+      locale: user.locale,
+      picture: user.picture,
+    });
+    navigation.navigate("HomeScreen");
+  }
 
   useEffect(() => {
     token &&
     getUserInfo(token)
       .then((user) => {
         console.log('user', user);
-        setUser(user)
-        
-        setUserInfo({
-          uid: user.id,
-          email: user.email,
-          family_name: user.family_name,
-          given_name: user.given_name,
-          name: user.name,
-          locale: user.locale,
-          picture: user.picture,
-        });
-        navigation.navigate("HomeScreen");
+        onSuccess(user);
       })
       .catch((error) => {
         console.log('error', error);
@@ -38,7 +39,6 @@ function SignIn() {
     <View style={{flex: 1, alignItems: "center", justifyContent: "center",}}>
       <Text>Welcome</Text>
       <Text></Text>
-      <Text>{user?.given_name}</Text>
       <Text></Text>
       <Button 
         title="Connect with Google"           
