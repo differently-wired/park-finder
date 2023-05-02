024854a017eba6462665d021b6b54e16eed13c17
ah28011992 <hedgecockaaron@gmail.com>
2023-05-02 15:12:07 +0100

    created map template with api key and maps function

---
+++ Components/TomTomMaps.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { WebView } from 'react-native-webview';
+import mapTemplate from '../templates/map-template';
+
+export default function TomTomMaps() {
+    let webRef = undefined;
+    let [mapCenter, setMapCenter] = useState('-121.913, 37.361');
+
+    const onButtonPress = () => {
+        const [lng, lat] = mapCenter.split(",");
+        webRef.injectJavaScript(`map.setCenter([${parseFloat(lng)}, ${parseFloat(lat)}])`);
+    }
+
+    const handleMapEvent = (event) => {
+        setMapCenter(event.nativeEvent.data)
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={setMapCenter}
+                    value={mapCenter}></TextInput>
+                <Button title="Set Center" onPress={onButtonPress}></Button>
+            </View>
+            <WebView
+                ref={(r) => (webRef = r)}
+                onMessage={handleMapEvent}
+                style={styles.map}
+                originWhitelist={['*']}
+                source={{ html: mapTemplate }}
+            />
+        </View>
+    );
+}
--- Screens/Homescreen.jsx
+++ Screens/Homescreen.jsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import TomTomMaps from '../Components/TomTomMaps';
 
 
 export default function Homescreen() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Hello world</Text>
+            <TomTomMaps />
         </View>
+
     );
 }
