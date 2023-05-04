1f8f9545f6582c291918b2b83ee8386e312b54e6
Kendrick Chan <kenchan0824@gmail.com>
2023-05-04 14:31:40 +0100

    add url to picture

--- utils/parkings.js
+++ utils/parkings.js
@@ -11,6 +11,7 @@ export async function addParking(parkObj) {
   // duration
   // reminder
   // notes
+  // pcitureUrl
 
   const parkRef = collection(FIRESTORE_DB, 'parkings')
   const parkDoc = {
