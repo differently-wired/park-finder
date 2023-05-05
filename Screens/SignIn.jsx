03dcccf2f599562234aa748fc439ebd3a14ab40e
Kendrick Chan <kenchan0824@gmail.com>
2023-05-05 06:59:50 +0100

    remove unused imports

--- Screens/SignIn.jsx
+++ Screens/SignIn.jsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Text,
   View,
   Image,
@@ -9,7 +8,6 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Keyboard,
-  Dimensions,
 } from "react-native";
 import { useContext, useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -28,7 +26,7 @@ function SignIn() {
   const { setUserInfo } = useContext(UserInfoContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [request, accessToken, promptAsyn] = useGoogleAuth();
+  const [request, accessToken, promptAsync] = useGoogleAuth();
   const navigation = useNavigation();
 
   function onSuccess(firebaseUser) {
@@ -103,12 +101,7 @@ function SignIn() {
         >
           <Text style={styles.buttonOutlineText}>Sign in</Text>
         </TouchableOpacity>
-        {/* <Button
-          title="Connect with Google"
-          disabled={!request}
-          onPress={() => promptAsyn()}
-        /> */}
-        <TouchableOpacity disabled={!request} onPress={() => promptAsyn()}>
+        <TouchableOpacity disabled={!request} onPress={() => promptAsync()}>
           <View style={styles.GoogleButton}>
             <Image
               style={styles.GoogleImage}
