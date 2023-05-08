85f976911a5dae5ebfd118a1c9047d089d54b3f0
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-05-08 17:20:10 +0100

    notifications now set to 2 seconds and are visible in background and foreground

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import * as Notifications from "expo-notifications";
+
 import MyStack from "./Routes/MyStack";
 import { UserInfoProvider } from "./contexts/UserInfo";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+    };
+  },
+});
+
 export default function App() {
+  useEffect(() => {
+    const getPermissions = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (status !== "granted") {
+        const { lastStatus } = await Notifications.requestPermissionsAsync();
+        finalStatus = lastStatus;
+      }
+      if (finalStatus !== "granted") {
+        console.log("Failed to get push token for push notification!");
+        return;
+      }
+    };
+    getPermissions();
+  }, []);
+
   return (
     <UserInfoProvider>
       <NavigationContainer>
