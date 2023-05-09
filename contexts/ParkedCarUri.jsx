998dd93266a4760cb453ee02fdb8812d9e809151
Keenan Gee <k33nang33@hotmail.co.uk>
2023-05-09 14:30:03 +0100

    added context to hold image uri locally

--- contexts/ParkedCarUri.jsx
+++ contexts/ParkedCarUri.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const ImageUriContext = createContext();
 
 export function ImageUriProvider({ children }) {
-  const [imageUri, setImageUri] = useState("hello");
+  const [imageUri, setImageUri] = useState(null);
 
   return (
     <ImageUriContext.Provider value={{ imageUri, setImageUri }}>
