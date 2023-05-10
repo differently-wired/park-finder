019736c23294991c43c1236d3038867885a8e3f5
ah28011992 <hedgecockaaron@gmail.com>
2023-05-10 02:03:23 +0100

    add purple rounded box to spinner and increased font size

--- Components/Loading_Spinner/Loading.js
+++ Components/Loading_Spinner/Loading.js
@@ -3,6 +3,8 @@ import { Text, View, StyleSheet, ActivityIndicator, Modal, Animated } from "reac
 
 function LoadingScreen() {
     const spinValue = new Animated.Value(0);
+    const colorValue = new Animated.Value(0);
+    const [currentColor, setCurrentColor] = React.useState("#6C21DC");
 
     const spin = spinValue.interpolate({
         inputRange: [0, 1],
@@ -10,24 +12,57 @@ function LoadingScreen() {
     });
 
     React.useEffect(() => {
-  
-        Animated.loop(
+        const spinAnimation = Animated.loop(
             Animated.timing(spinValue, {
                 toValue: 1,
                 duration: 1500,
                 useNativeDriver: true,
             })
-        ).start();
+        );
+
+        const colorAnimation = Animated.loop(
+            Animated.timing(colorValue, {
+                toValue: 1,
+                duration: 1500,
+                useNativeDriver: false,
+            })
+        );
+
+        const timer = setInterval(() => {
+            setCurrentColor(
+                colorValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ["#6C21DC", "#8B2CBF"], // Blue to purple colors
+                })
+            );
+        }, 1500);
+
+        spinAnimation.start();
+        colorAnimation.start();
+
+        return () => {
+            spinAnimation.stop();
+            colorAnimation.stop();
+            clearInterval(timer);
+        };
     }, []);
 
     return (
         <Modal transparent={true} animationType="none">
             <View style={styles.overlay}>
                 <View style={styles.container}>
-                    <Animated.View style={{ transform: [{ rotate: spin }] }}>
-                        <ActivityIndicator color="#6C21DC" size="large" />
-                    </Animated.View>
                     <Text style={styles.loadingText}>Loading...</Text>
+                    <View style={styles.spinnerContainer}>
+                        <Animated.View
+                            style={{
+                                transform: [{ rotate: spin }],
+                                backgroundColor: currentColor,
+                                borderRadius: 8, // Rounded edges
+                            }}
+                        >
+                            <ActivityIndicator color="white" size="large" />
+                        </Animated.View>
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -47,9 +82,16 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         alignItems: "center",
     },
+    spinnerContainer: {
+        marginTop: 16,
+        width: 40,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center",
+    },
     loadingText: {
-        fontSize: 18,
-        marginTop: 10,
+        fontSize: 24,
+        marginBottom: 10,
     },
 });
 
